e9953914f71ee5870dbabe7bd34d1912953b4767
nikhat shaikh <niksk@821>
2023-11-04 11:19:53 +0530

    files addded

---
+++ src/app/api/send/route.js
@@ -0,0 +1,30 @@
+// import { EmailTemplate } from '../../../components/EmailTemplate';
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
+
+export async function POST(req, res) {
+  const { body } = req;
+  const { email, subject, message } = body;
+  try {
+    const data = await resend.emails.send({
+      from: fromEmail,
+      to: ["nik842196@gmail.com"],
+      subject: "Hello world",
+      react: (
+        <>
+          <h1>{subject}</h1>
+          <p>thank you for contacting!</p>
+          <p>New Submitted</p>
+          <p>{message}</p>
+        </>
+      ),
+    });
+
+    return NextResponse.json(data);
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
